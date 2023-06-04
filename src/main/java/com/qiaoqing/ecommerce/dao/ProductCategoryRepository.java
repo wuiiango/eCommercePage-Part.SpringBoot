8cb159babd1c0c029b852afc18d05a3344f199c4
Qiaoqing Wu <wu000302@algonquinlive.com>
2023-06-04 16:35:05 -0400

    Add CrossOrigin for specific port number

--- src/main/java/com/qiaoqing/ecommerce/dao/ProductCategoryRepository.java
+++ src/main/java/com/qiaoqing/ecommerce/dao/ProductCategoryRepository.java
@@ -3,8 +3,10 @@ package com.qiaoqing.ecommerce.dao;
 import com.qiaoqing.ecommerce.entity.ProductCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 // specify given data, "productCategory": name of JSON entry
+@CrossOrigin("http://localhost:4200")
 @RepositoryRestResource(collectionResourceRel = "productCategory", path = "product-category")
 public interface ProductCategoryRepository extends JpaRepository<ProductCategory, Long> {
 
